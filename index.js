4a8eb115a1bbc8a84310606b791e7654794f3de0
jogadorks <76264738+jogadorks@users.noreply.github.com>
2021-03-06 00:35:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -331,7 +331,7 @@ async function starts() {
 			const mod = [ownerNumber,"5569984423608@s.whatsapp.net"]//ubah nomor lo
 			const adminbotnumber = ["5569984423608@s.whatsapp.net"]//ubah nomor lo
 			const frendsowner = ["5569984423608@s.whatsapp.net"]//ubah nomor lo
-			const premium = ["5522996215481@s.whatsapp.net"]
+			const premium = ["5569984423608@s.whatsapp.net"]
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
